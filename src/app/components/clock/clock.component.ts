c438e893bfca3b9fcbb46f30bd675db6a20b1c93
deblina_pattanayak <deblina.pattanayak@mheducation.com>
2019-07-14 01:55:43 +0530

    initial commit

---
+++ src/app/components/clock/clock.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+
+@Component({
+  selector: 'app-clock',
+  templateUrl: './clock.component.html',
+  styleUrls: ['./clock.component.scss']
+})
+export class ClockComponent implements OnInit {
+  public time: any;
+  constructor() { }
+
+  ngOnInit() {
+    setInterval(this.clock.bind(this) , 1000);
+  }
+
+  public date() {
+    return moment().format("MMM Do YYYY");
+  }
+
+  public clock() {
+    this.time = moment().format("HH:mm:ss");
+    console.log(this.time);
+  }
+
+}
